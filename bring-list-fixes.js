ed45d74adf0a16b340f51e8c4339fdbc0141ac48
katonakri <katonakrii@gmail.com>
2026-08-17 12:28:41 +0200

    Refine Mit hozzunk item icons

---
+++ bring-list-fixes.js
@@ -0,0 +1,12 @@
+/* Mit hozzunk? — icon refinements */
+Object.assign(ICON_PATHS, {
+  grill: '<path d="M4.5 8.5h15"/><path d="M5.5 9.5c.5 4.2 2.9 6.5 6.5 6.5s6-2.3 6.5-6.5"/><path d="M12 16v5"/><path d="M8.5 21h7"/><path d="M7 5.5v2.5M12 4v4M17 5.5v2.5"/>',
+  salad: '<path d="M4 11h16c0 5-3.2 8-8 8s-8-3-8-8z"/><path d="M7 11c.5-3 2.3-5 5-5s4.5 2 5 5"/><path d="M9 8.5c.5-1.8 1.5-2.8 3-3.5M15 8.5c-.5-1.8-1.5-2.8-3-3.5"/>',
+  drink: '<path d="M6.5 5h11l-1.2 15H7.7L6.5 5z"/><path d="M9 3h6"/><path d="M12 3v2"/><path d="M15.5 8c1.8-1.2 3.2-2.5 3.5-4"/><path d="M8.5 10h7"/>',
+  chips: '<path d="M5 8.5c2-1.5 4-1.5 7 0s5 1.5 7 0v8c-2 1.5-4 1.5-7 0s-5-1.5-7 0v-8z"/><path d="M8 10.5v3M12 10v3.5M16 10.5v3"/>',
+  plate: '<ellipse cx="12" cy="13" rx="8.5" ry="5.5"/><ellipse cx="12" cy="13" rx="5" ry="2.5"/><path d="M5 13v1.5c0 3.5 3 5.5 7 5.5s7-2 7-5.5V13"/>',
+  cup: '<path d="M6.5 6h11l-1 14h-9l-1-14z"/><path d="M8.5 6c0-2 1.3-3 3.5-3s3.5 1 3.5 3"/><path d="M9 10h6"/>',
+  bottle: '<path d="M10 4h4v3l2 2v11H8V9l2-2V4z"/><path d="M10 4h4M9 13h6M11 16h2"/>',
+  towel: '<path d="M6 4h12v16H6z"/><path d="M9 4v16M15 4v16M6 8h12M6 16h12"/>',
+  bag: '<path d="M6 8h12l1 12H5L6 8z"/><path d="M9 8V6a3 3 0 0 1 6 0v2"/><path d="M9 12h6"/>'
+});
